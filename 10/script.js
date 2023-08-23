4017dc29449d117b79bed9212d27f38f024c8229
Kirill Kolesnikov <116028314+arizonec@users.noreply.github.com>
2023-08-23 14:53:34 +0300

    readme fix

--- 10/script.js
+++ 10/script.js
@@ -1,138 +1,169 @@
-const convertToJSON = (str) => {
-    let currentIndex = 0;
+
+function parse(str) {
+    let i = 0;
+
+    function parseObject() { 
+        if(str[i] !== '{'){
+            throw new Error('Строка не является корректным JSON объектом');
+
+        }
+        i++;
+        const obj = {};
     
-    try {
-      const jsonObject = parseObject();
-      if (currentIndex !== str.length) {
-        throw new Error('Строка не является валидным JSON');
-      }
-      return jsonObject;
-    } catch (error) {
-      console.error('Ошибка парсинга JSON:', error);
-      return null;
-    }
+        while (i < str.length && str[i] !== '}') { 
+            const key = parseString(); 
+
+            if (str[i] !== ':') {
+                throw new Error('Строка не является корректным JSON объектом');
+            }
     
-    function parseObject() {
-      if (str[currentIndex] !== '{') {
-        throw new Error('Строка не является валидным JSON объектом');
-      }
-      
-      currentIndex++;
-      const obj = {};
-      
-      while (currentIndex < str.length && str[currentIndex] !== '}') {
-        const key = parseString();
-        
-        if (str[currentIndex] !== ':') {
-          throw new Error('Строка не является валидным JSON объектом');
+            i++;
+    
+            const value = parseValue();
+            obj[key] = value; 
+    
+          
+            if (str[i] === ',') {
+                i++;
+            }
         }
         
-        currentIndex++;
-        const value = parseValue();
-        obj[key] = value;
-        
-        if (str[currentIndex] === ',') {
-          currentIndex++;
+        if (str[i] !== '}') {
+            throw new Error('Строка не является корректным JSON объектом');
         }
+          
+        i++;
+        return obj;
       }
-      
-      if (str[currentIndex] !== '}') {
-        throw new Error('Строка не является валидным JSON объектом');
-      }
-      
-      currentIndex++;
-      return obj;
-    }
-    
-    function parseArray() {
-      if (str[currentIndex] !== '[') {
-        throw new Error('Строка не является валидным JSON массивом');
-      }
-      
-      currentIndex++;
-      const arr = [];
-      
-      while (currentIndex < str.length && str[currentIndex] !== ']') {
-        const value = parseValue();
-        arr.push(value);
-        
-        if (str[currentIndex] === ',') {
-          currentIndex++;
-        }
-      }
-      
-      if (str[currentIndex] !== ']') {
-        throw new Error('Строка не является валидным JSON массивом');
-      }
-      
-      currentIndex++;
-      return arr;
-    }
-    
-    function parseString() {
-      if (str[currentIndex] !== '"') {
-        throw new Error('Строка не является валидной JSON строкой');
-      }
-      
-      currentIndex++;
-      let result = '';
-      
-      while (currentIndex < str.length && str[currentIndex] !== '"') {
-        result += str[currentIndex];
-        currentIndex++;
-      }
-      
-      if (str[currentIndex] !== '"') {
-        throw new Error('Строка не является валидной JSON строкой');
-      }
-      
-      currentIndex++;
-      return result;
-    }
     
     function parseValue() {
-      if (str[currentIndex] === '{') {
-        return parseObject();
-      } else if (str[currentIndex] === '[') {
-        return parseArray();
-      } else if (str[currentIndex] === '"') {
-        return parseString();
-      } else if (str[currentIndex] === 't' && str.substring(currentIndex, currentIndex + 4) === 'true') {
-        currentIndex += 4;
-        return true;
-      } else if (str[currentIndex] === 'f' && str.substring(currentIndex, currentIndex + 5) === 'false') {
-        currentIndex += 5;
-        return false;
-      } else if (str[currentIndex] === 'n' && str.substring(currentIndex, currentIndex + 4) === 'null') {
-        currentIndex += 4;
-        return null;
-      } else {
-        const number = parseNumber();
-        return number;
-      }
+        if (str[i] === '{') { 
+            return parseObject();
+        }else if (str[i] === '[') { 
+            return parseArray();
+        }else if (str[i] === '"') { 
+            return parseString();
+        }else if (str[i] === 't' && str.substring(i, i + 4) === 'true') {
+            i += 4;
+            return true;
+        }else if (str[i] === 'f' && str.substring(i, i + 5) === 'false') {
+            i += 5;
+            return false;
+        }else if (str[i] === 'n' && str.substring(i, i + 4) === 'null') {
+            i += 4;
+            return null;
+        }else { 
+            const number = parseNumber();
+            return number;
+        } 
     }
-    
+  
+    function parseArray() { 
+        if (str[i] !== '[') {
+            throw new Error('Строка не является корректным JSON массивом');
+        }
+        i++;
+        const arr = [];
+  
+        while (i < str.length) { 
+            if (str[i] === ' ' || str[i] === ',') {
+                i++;
+                continue;
+            }
+  
+            if (str[i] === ']') {
+                i++;
+                return arr;
+            }
+  
+            const value = parseValue(); 
+            arr.push(value); 
+  
+            if (str[i] === ' ' || str[i] === ',') {
+                i++;
+            }
+        }
+        throw new Error('Строка не является корректным JSON массивом');
+    }
+  
+    function parseString() {
+        if (str[i] !== '"') {
+            throw new Error('Строка не является корректной JSON строкой');
+        }  
+
+
+        i++;  
+        let result = '';
+        let escape = false;
+     
+        while (i < str.length) {
+            if (escape) { 
+                if (str[i] === '"' || str[i] === '\\' || str[i] === '/') {
+                    result += str[i];
+                }else if (str[i] === 'b') { 
+                    result += '\b';
+                }else if (str[i] === 'f') { 
+                    result += '\f';
+                }else if (str[i] === 'n') { 
+                    result += '\n';
+                }else if (str[i] === 'r') { 
+                    result += '\r';
+                }else if (str[i] === 't') { 
+                    result += '\t';
+                }else { 
+                    throw new Error('Ошибка экранирования!');
+                }
+                escape = false; 
+            }else {
+                if (str[i] === '"') { 
+                    i++;
+                    return result;
+                }else if (str[i] === '\\') { 
+                    escape = true;
+                }else { 
+                    result += str[i];
+                }
+            }
+  
+            i++;
+        }
+      
+        throw new Error('Ошибка строки, проверьте строку на правильность!');
+    }
+  
     function parseNumber() {
-      let number = '';
-      
-      while (currentIndex < str.length && isDigit(str[currentIndex])) {
-        number += str[currentIndex];
-        currentIndex++;
-      }
-      
-      if (number === '') {
-        throw new Error('Строка не является валидным JSON числом');
-      }
-      
-      return parseFloat(number);
+        let number = '';
+        
+        while (i < str.length && isDigit(str[i])) {
+            number += str[i];
+            i++;
+        }
+        
+        if (number === '') {
+            throw new Error('Строка не является корректным JSON числом');
+        }
+        return Number(number);
     }
-    
+      
     function isDigit(char) {
-      return /[0-9]/.test(char);
+        return /[0-9]/.test(char);
+    }
+
+    return parseObject();
+}
+  
+const tryJSON = (string) => {
+    try {
+        const jsonObject = parse(string);
+        console.log(jsonObject);
+    }catch (error) {
+        console.error(error);
     }
 }
 
-console.log(convertToJSON('{"name":"alex","sizes":[1,2,3,4]}'));
-console.log(convertToJSON('{"name":"alex","sizes":[1,2,3,4}'));
-console.log(convertToJSON('{"name":"alex","sizes":[1,2,3,4]'));
-console.log(convertToJSON('{"name":"alex","sizes":[1,2,3,4],"type":true}'));
+const string = '{"name":"Alex","sizes":[1,2,3,4], "cities":}';
+const string2 = '{"name":"Thomas","birthDay":"1995-04-05T00:00:00.000Z"}';
+
+
+tryJSON(string);
+tryJSON(string2);
